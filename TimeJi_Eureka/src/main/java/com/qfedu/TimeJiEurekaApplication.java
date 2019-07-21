60403bcdab2425f86a93a4e459bfb91da5ec886d
zhangqi234110 <314890945@qq.com>
2019-07-21 17:34:22 +0800

    注册中心

---
+++ TimeJi_Eureka/src/main/java/com/qfedu/TimeJiEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.qfedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TimeJiEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeJiEurekaApplication.class, args);
+	}
+
+}
